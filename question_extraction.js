14b02041156ff141e1cc53e302b9ad1ad0b04f79
theorchardpill <show.co.kr@gmail.com>
2025-03-23 19:56:59 +0900

    Update question_extraction.js

--- question_extraction.js
+++ question_extraction.js
@@ -1,3 +1,34 @@
+/**
+ * ChatGPT 질문추출도구.js | ChatGPT Question Extractor
+ * Version: 1.0.0
+ * Description: Extracts only the user's questions from ChatGPT-exported HTML files and displays them in a simple, readable format.
+ *
+ * Author: Vincent
+ * Blog: https://mikkorimimi.blogspot.com/
+ *
+ * License: MIT License
+ *
+ * Copyright (c) 2025 Vincent
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+
 (function(){
   "use strict";
 
